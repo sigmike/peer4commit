73a27acba1df696dea4206739235a757385b85eb
avanesov <avanesov.art@gmail.com>
2014-03-25 03:06:15 +0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,49 +1,55 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :trackable, :validatable
-
-  devise :omniauthable, :omniauth_providers => [:github]
-
-  validates :bitcoin_address, :bitcoin_address => true
-
-  has_many :tips
-
-  def github_url
-    "https://github.com/#{nickname}"
-  end
-
-  def balance
-  	tips.unpaid.sum(:amount)
-  end
-
-  after_create :generate_login_token!
-  def generate_login_token!
-    if login_token.blank?
-      self.update login_token: SecureRandom.urlsafe_base64
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable
+    devise :database_authenticatable, :registerable, :recoverable,
+           :rememberable, :trackable, :validatable
+  
+    devise :omniauthable, :omniauth_providers => [:github]
+  
+    # Validations
+    validates :bitcoin_address, :bitcoin_address => true
+  
+    # Associations
+    has_many :tips
+  
+    # Callbacks
+    before_create :set_login_token!, unless: :login_token?
+  
+    # Instance Methods
+    def github_url
+      "https://github.com/#{nickname}"
+    end
+  
+    def balance
+      tips.unpaid.sum(:amount)
+    end
+  
+    def full_name
+      name.presence || nickname.presence || email
+    end
+  
+    # Class Methods
+    def self.update_cache
+      find_each do |user|
+        user.update commits_count: user.tips.count
+        user.update withdrawn_amount: user.tips.paid.sum(:amount)
+      end
+    end
+  
+   def self.create_with_omniauth!(auth_info)
+     generated_password = Devise.friendly_token.first(Devise.password_length.min)
+ 
+     create!( email:    auth_info.email,
+              password: generated_password,
+              nickname: auth_info.nickname)
+   end
+ 
+    private
+  
+    def set_login_token!
+      loop do
+        self.login_token = SecureRandom.urlsafe_base64
+        break login_token unless User.exists?(login_token: login_token)
+      end
     end
   end
-
-  def full_name
-    if !name.blank?
-      name
-    elsif !nickname.blank?
-      nickname
-    else
-      email
-    end
-  end
-
-  def self.update_cache
-    commits_counts = Tip.group(:user_id).count
-    paid_sums = Tip.paid.group(:user_id).sum(:amount)
-
-    find_each do |user|
-      user.commits_count = commits_counts[user.id] || 0
-      user.withdrawn_amount = paid_sums[user.id] || 0
-      user.save
-    end
-  end
-
-end
